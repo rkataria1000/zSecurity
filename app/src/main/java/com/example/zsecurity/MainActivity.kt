35d0225b73edaead7be1788b100d93e630f42825
Rahul Kataria <rkataria5000@gmail.com>
2023-02-21 17:24:51 +0530

    initial project

---
+++ app/src/main/java/com/example/zsecurity/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.zsecurity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val bottomBar = findViewById<BottomNavigationView>(R.id.bottom_nav_bar)
+        bottomBar.setOnItemSelectedListener {
+            if (it.itemId == R.id.Shield)
+            {
+                inflateFragment(GuardFragment.newInstance())
+            }
+            else if (it.itemId == R.id.nav_home)
+            {
+                inflateFragment(HomeFragment.newInstance())
+            }
+            else if (it.itemId == R.id.dash_board)
+            {
+                inflateFragment(DashboardFragment.newInstance())
+            }
+            else
+            {
+                inflateFragment(ProfileFragment.newInstance())
+            }
+
+            true
+        }
+
+
+
+    }
+
+
+
+    private fun inflateFragment(newInstance: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container, newInstance)
+        transaction.commit()
+
+    }
+
+}
